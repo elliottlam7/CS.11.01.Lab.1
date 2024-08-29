a0a454a1b6f7f66e1e6adeea47faf5a3533d87fb
elliottlam <elliottla2027@student.cis.edu.hk>
2024-08-29 20:19:01 +0800

    elliott lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scannerAge = new Scanner(System.in);
+        System.out.println("Welcome! This program should help you determine what age person you should date. \nTo start, please enter your age: ");
+        int age = -1;
+        while (age < 0) {
+            System.out.println("Please enter a valid number.");
+            try {
+                age = Integer.parseInt(scannerAge.nextLine());
+            }
+            catch(NumberFormatException e) {
+            }
+        }
+        int minDatingAge = ((age / 2) + 7);
+        System.out.println("Alright! " + age + "-year olds should date someone who is at least " + minDatingAge + "-years old." + " \nThanks for using this program!");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+
+        Scanner scannerBook = new Scanner(System.in);
+        System.out.println("Welcome! This program tells you how many pages you should read before giving up on a book. \nTo start, please enter your age: ");
+        int age = -1;
+        while (age < 0) {
+            System.out.println("Please enter a valid number.");
+            try {
+                age = Integer.parseInt(scannerBook.nextLine());
+            }
+            catch(NumberFormatException e) {
+            }
+        }
+        int minPagesRead = 100 - age;
+        System.out.println("Alright! " + age + "-year olds should read at least " + minPagesRead + " pages before giving up on a book." + " \nThanks for using this program!");
 
     }
 }
